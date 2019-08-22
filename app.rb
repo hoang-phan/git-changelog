702b66fdd4f3c25336123f226641e0fba05ce733
Hoang Phan <hoangphanbk10@gmail.com>
2019-08-22 11:22:15 +0700

    [T1] Fix

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require 'httparty'
 post '/payload' do
   content_type :json
   payload = JSON.parse(request.body.read)
-
   pull_request = payload['pull_request']
+
   if payload['action'] == 'closed' && pull_request['merged']
     contents = Github::Client::Repos::Contents.new(login: ENV['GITHUB_USERNAME'], password: ENV['GITHUB_PASSWORD'])
     file = contents.find(ENV['GITHUB_ORGANIZATION'], ENV['GITHUB_REPO'], 'CHANGELOG.md')
